7d36fe55f12b674d01b8d287903f2e4fd91d82d3
MyNameJaeff <fredrik.andersson@elev.ga.ntig.se>
2022-10-14 12:59:47 +0200

    La till uppgift 3

--- script/script.js
+++ script/script.js
@@ -1,9 +1,10 @@
 function randomNumbers(){    //Skapar en array med 10 nummer mellan 1-100 som sedan skickas ut som alert
     let hundredRand = []
-    for(let i = 0; i<10; i++){
+    for(let i = 0; i<10;){
         let tal = Math.floor(Math.random() * 101)
         if(!hundredRand.includes(tal)){ //Ifall tal inte finns i array så tar lägger den till ett tal i den
             hundredRand.push(tal)
+            i++
         }
         else{
             console.log("Duplicate: " + tal)
@@ -13,30 +14,45 @@ function randomNumbers(){    //Skapar en array med 10 nummer mellan 1-100 som se
     alert("Sorted: " + hundredRand.sort(function(a, b){return a-b})) //Sorterar talen från minst till störts
 }
 const verifyForm = () =>{    //Kollar ifall allting är korrekt i första formuläret
+    let username = document.getElementById("username").value
     let password1 = document.getElementById("password1").value
     let password2 = document.getElementById("password2").value
     let email = document.getElementById("email").value
     let checkbox = document.getElementById("agreement")
-    if(password1.length < 6){ //Ifall lösenordet är mindre än 6 bokstäver ger den error medelande
-        alert("Your password has to be atleast 6 letters long")
-    }
-    else{
-        if(password2 != password1){ //Ifall lösenorden inte är likadana så ger den error medelande
-            alert("Your passwords do not match")
+    if(username.length > 0){
+        if(password1.length < 6){ //Ifall lösenordet är mindre än 6 bokstäver ger den error medelande
+            alert("Your password has to be atleast 6 letters long")
         }
         else{
-            if(email[0] == "@" | email.slice(-1) == "@"){ //Ifall första eller sista symol är @ så ge error medelande
-                alert("That's not a correct email adress")
+            if(password2 != password1){ //Ifall lösenorden inte är likadana så ger den error medelande
+                alert("Your passwords do not match")
             }
             else{
-                if(checkbox.checked){
-                    document.location.href = "./thanks.html"
+                if(checkEmail(email) == false){ //Kör funktionen checkEmail
+                    alert("That's not a correct email adress")
                 }
                 else{
-                    alert("You have to check the box first")
+                    if(checkbox.checked){
+                        document.location.href = "./thanks.html" //Byter sida till thanks.html
+                    }
+                    else{
+                        alert("You have to check the box first")
+                    }                
                 }
             }
         }
+    }
+    else{
+        alert("That is not a correct username")
+    }
+}
+function checkEmail(email){ //Kollar ifall resultat blev false och ifall det finns ett @ i början eller slutet av emailen
+    let result = email.includes("@");//Returnerar false ifall emailen inte includerar ett @
+    if(result == false || (email[0] == "@" || email.slice(-1) == "@")){
+        return false;
+    }
+    else{
+        return true;
     }
 }
 //randomNumbers()
